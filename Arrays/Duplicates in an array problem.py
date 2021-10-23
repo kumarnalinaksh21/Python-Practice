0ed46432abe40b3e1a029b0f6ff4e021704313cb
Kumar Nalinaksh <kumarnalinaksh21@gmail.com>
2021-10-24 02:20:19 +0530

    Update Duplicates in an array problem.py

--- Arrays/Duplicates in an array problem.py
+++ Arrays/Duplicates in an array problem.py
@@ -1,4 +1,4 @@
-################ Question ######################################
+############################# Question ######################################
 # The problem is that we want to find duplicates in a 
 # one-dimensional array of integers in O(N) running time where 
 # the integer values are smaller than the length of the array!
@@ -7,27 +7,46 @@
 # Note: the array can not contain items smaller than 0 and items 
 # with values greater than the size of the list. This is how we can 
 # achieve O(N) linear running time complexity!
-################################################################
+##############################################################################
 
 
 
-def check_duplicate_entries(var_list):
-    duplicate_list = []
-    for i in var_list:
-        if i < 0 or i > len(var_list):
-            print("the array can not contain items smaller than 0 and items with values greater than the size of the list.")
+def brute_force_approach(var_list):
+    i = 0
+    duplicate = []
+    while i < len(var_list):
+        j = i+1
+        while j < len(var_list):
+            if var_list[i] == var_list[j]:
+                duplicate.append(var_list[i])
+            j = j + 1
+        i = i + 1
+    print(set(duplicate))
+
+
+def hash_maps_approach(var_list):
+    duplicate = {}
+    for element in var_list:
+        try:
+            duplicate[element] = duplicate[element] + 1
+        except:
+            duplicate[element] = 1
+    for element in duplicate:
+        if duplicate[element] > 1:
+            print(element, end = " ")
+
+
+def absolute_value_approach(var_list):
+    for element in var_list:
+        if var_list[abs(element)] >= 0:
+            var_list[abs(element)] = -var_list[abs(element)]
         else:
-            while i < len(var_list) - 1:
-                if var_list ()[i] == var_list.sort()[i + 1]:
-                    duplicate_list.append(var_list[i])
-                i = i+1
-   
-#    if len(duplicate_list) > 0:
-#        print(duplicate_list, "are the duplicates in ", var_list)
-#    else:
-#        print("no duplicates found in ", var_list)
+            print("Duplicate element found ", abs(element))
+
 
 
 if __name__ == "__main__":
-    var_list = [1, 2, 3, 1, 5]
-    check_duplicate_entries(var_list)
+    var_list = [1, 2, 1, 2, 4]
+    #brute_force_approach(var_list)
+    hash_maps_approach(var_list)
+    #absolute_value_approach(var_list)
